20f31713de01119f573a75283ef1e148f344ce3e
thinhsuy2908 <phan90539@gmail.com>
2025-02-12 20:19:13 +0700

    Update function.h

--- function.h
+++ function.h
@@ -1,6 +1,5 @@
-void listUser();
-void addStudent();
-void displayUsers();
-void menuAdmin();
-void AddUser();
-void runMainMenu();
+void AddUser(struct User* user, int* uses);
+void findUser(struct User* user, int uses);
+void listUser(struct User *user, int uses);
+void menuAdmin(struct User* user, int* uses);
+void showSystemMenu(struct User *user, int* uses);
