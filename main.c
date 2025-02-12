7df027b78cae34dc6d84ef442633db643965f386
thinhsuy2908 <phan90539@gmail.com>
2025-02-12 20:17:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include"function.h"
+#include "datatype.h"
+#include "function.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	addStudent();
+	int uses =0;
+	struct User user[100];
+	showSystemMenu(user,&uses);
 	return 0;
 }
