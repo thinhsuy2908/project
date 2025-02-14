1afa829c269352c67baa49bf2066acf02e8da418
thinhsuy2908 <phan90539@gmail.com>
2025-02-15 00:29:24 +0700

    Create function.h

---
+++ include/function.h
@@ -0,0 +1,13 @@
+#include "./datatype.h"
+void AddUser(struct User* user, int* uses);
+void findUser(struct User* user, int uses);
+void findUserName (struct User* user, int uses);
+void listUser(struct User *user, int uses);
+void menuAdmin(struct User* user, int* uses);
+void login();
+void goBack(struct User *user, int* uses);
+void idLock(struct User *user, int* uses);
+void arrange(struct User *user, int* uses);
+void showSystemMenu(struct User *user, int* uses);
+void fileRead(struct User *user, int* uses);
+void fileWrite(struct User *user, int uses);
