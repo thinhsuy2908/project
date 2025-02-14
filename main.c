5b0ac8ef639653261f23a7bccc9940063380034f
thinhsuy2908 <phan90539@gmail.com>
2025-02-15 00:26:55 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "datatype.h"
-#include "function.h"
+#include "./include/datatype.h"
+#include "./include/function.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int uses =0;
-	struct User user[100];
-	showSystemMenu(user,&uses);
-	return 0;
+int main() {
+    int uses =0;
+    struct User user[100];
+    printf("pass");
+    showSystemMenu(user,&uses);
+    return 0;
 }
